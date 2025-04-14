0cd009d0d71ffaea2d7f4e0b17dede0bbe6d25c8
Andd-Y <ff.crack2.0.0.6@gmail.com>
2025-04-14 18:22:09 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("reserva-form").addEventListener("misubt", function(event) {event.preventDefault();
+alert("mensaje enviado con exito")
+})
+
+document.getElementById(`contacto-form`).addEventListener(`submit`, function(event) {event.preventDefault();
+    alert(`mensaje enviado con exito`);
+})
